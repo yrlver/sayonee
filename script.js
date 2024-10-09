723a5bfb59c6e24b5cddb58ba066d36871ac8524
yrlver <izztybharddn@gmail.com>
2024-10-09 21:01:37 +0800

    Update script.js

--- script.js
+++ script.js
@@ -10,11 +10,6 @@ const gif = document.querySelector(".gif");
 const yesBtn = document.querySelector(".yes-btn");
 const noBtn = document.querySelector(".no-btn");
 
-yesBtn.addEventListener("click", () => {
-  question.innerHTML = "Aaaaa, I like you too";
-  gif.src =
-    "https://raw.githubusercontent.com/DzarelDeveloper/Img/main/gif.webp";
-});
 
 noBtn.addEventListener("mouseover", () => {
   const noBtnRect = noBtn.getBoundingClientRect();
